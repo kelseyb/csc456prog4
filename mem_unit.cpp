7f57418fe457378987c82f05e86ac8417d365b90
Shaun Gruenig <shaun.gruenig@mines.sdsmt.edu>
2014-05-02 14:46:18 -0600

    Shaun Lateristist commit

--- mem_unit.cpp
+++ mem_unit.cpp
@@ -2,18 +2,37 @@
 
 
 
-
+///////////////////////////////////////////////////////////////////////////////
+//
+// mem_unit::mem_unit function
+//
+// author: Shaun Gruenig
+//
+// description: This constructor simply initializes all elements in the 
+// 		block of memory to 0.
+//
+///////////////////////////////////////////////////////////////////////////////
 mem_unit::mem_unit()
 {
 	//initialize block of memory to all zeros
 	for(int i = 0; i < MEM_SIZE; i++)
-		mem[i] = ERROR;
+		mem[i] = 0;
 }
 
-mem_unit::~mem_unit()
-{
-}
 
+///////////////////////////////////////////////////////////////////////////////
+//
+// mem_unit::write function
+//
+// author: Shaun Gruenig
+//
+// description: This constructor simply initializes all elements in the 
+// 		block of memory to 0.
+//
+// parameters:  d - data to be written
+//		addr - address to write to
+//
+///////////////////////////////////////////////////////////////////////////////
 bool mem_unit::write(data d, address addr)
 {
 	mem[addr] = d;
